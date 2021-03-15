4171d5cc132ab76da54f7760a45e326ee1d47476
William Bugenis <wbugenis@gmail.com>
2021-03-15 10:50:42 -0400

    commit tree 2a4fdf1881e3a665d3f29d76de0d6d655eaabfa3

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+    <h2>About Me</h2>
+    <p>I'm a human being. I have interests and participate in activities.</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
